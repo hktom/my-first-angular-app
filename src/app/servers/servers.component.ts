5a8114a411f514f8be81ddca85d40d0de844b074
tom hikari <tomprescott1565@gmail.com>
2024-01-17 10:27:40 +0100

    Refactor component structure and add server creation functionality

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  styleUrl: './servers.component.css',
 })
 export class ServersComponent {
+  alowServer = true;
+  serverCreationStatus = 'No server was created!';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
+  constructor() {
+    setTimeout(() => {
+      this.alowServer = false;
+    }, 4000);
+  }
+
+  onCreateServer() {
+    // this.serverCreationStatus = 'Server was created!';
+    this.servers.push(this.serverCreationStatus);
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverCreationStatus = (<HTMLInputElement>event.target)
+      .value as string;
+  }
 }
